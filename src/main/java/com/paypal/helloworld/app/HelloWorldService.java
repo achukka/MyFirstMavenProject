f2b4d1abb11d578c91090c15f27dafc2967dfc5d
Aditya Chukka <adityaharish626@gmail.com>
2015-03-03 11:42:00 +0530

    Removed Unnecessary Xml name space definitions. Removed Context Scan from spring file

--- src/main/java/com/paypal/helloworld/app/HelloWorldService.java
+++ src/main/java/com/paypal/helloworld/app/HelloWorldService.java
@@ -1,12 +1,10 @@
 package com.paypal.helloworld.app;
 
-import org.springframework.stereotype.Service;
 
 /**
  * Hello world!
  *
  */
-@Service("helloWorldService")
 public class HelloWorldService 
 {
     private String message;
